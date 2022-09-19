d3f1a9c94537bad247cb5e65fd501dda7944d088
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2022-09-19 19:53:52 +0530

    Create afk.js

---
+++ plugins/afk.js
@@ -0,0 +1,33 @@
+const { bot } = require('../lib/')
+
+global.AFK = {
+	isAfk: false,
+	reason: false,
+	lastseen: 0,
+}
+
+bot(
+	{
+		pattern: 'afk ?(.*)',
+		fromMe: true,
+		desc: 'away from keyboard',
+		type: 'misc',
+	},
+	async (message, match) => {
+		if (!global.AFK.isAfk && !match)
+			return await message.send(
+				'උදාහරණය : මගේ හිමිකරු AFK\n අවසන් වරට දුටුවේ #latseen ට පෙර\nAFK අක්‍රිය කිරීමට නැවත පණිවිඩයක් යවන්න'
+			)
+		if (!global.AFK.isAfk) {
+			if (match) global.AFK.reason = match
+			global.AFK.isAfk = true
+			global.AFK.lastseen = Math.round(new Date().getTime() / 1000)
+			return await message.send(
+				match.replace(
+					'#lastseen',
+					Math.round(new Date().getTime() / 1000) - global.AFK.lastseen
+				)
+			)
+		}
+	}
+)
